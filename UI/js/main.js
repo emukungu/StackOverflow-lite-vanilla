894934d7a95d2ce7493567066c4c7fc775fba205
emukungu <enmukungu@gmail.com>
2018-08-12 11:19:38 +0300

    create the signin modal page[Delivers #159713530]

---
+++ UI/js/main.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var modal = document.getElementById('modal');
+
+    var signin = document.getElementById('signin');
+    var signup = document.getElementsByClassName("signupForm")[0];
+    var close = document.getElementsByClassName("close")[0];
+    var introduction = document.getElementsByClassName("introduction")[0];
+
+    signin.onclick = function() {
+        modal.style.display = "block";
+        signup.style.display = "none";
+        introduction.style.display = "none";
+    }
+
+    close.onclick = function(){
+        modal.style.display = "none";
+        signup.style.display = "block";
+        introduction.style.display = "block";
+    }
+}
